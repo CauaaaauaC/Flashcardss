cdceadc49be453fe545a4764fa6c70d66f7ecb45
CauaaaauaC <cagasparoto5@gmail.com>
2024-11-29 19:32:49 -0300

    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Educação financeira',
+    'O que precisa para ser um MEI?',
+    'Ter apenas um empregado e faturar até R$81 mil ao ano'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a formula de arranjo?',
+    'A = n!/(n-k)!'
+)
+
+criaCartao(
+    'Física',
+    'Quando jogamos um objeto para cima, ele cai devido a?',
+    'Gravidade da terra (9,8m/s²)'
+)
+
+criaCartao(
+    'Educação financeira',
+    'Quais são os tipos de notas fiscais?',
+    'NF-e, NFS-e e NFC-e'
+)
